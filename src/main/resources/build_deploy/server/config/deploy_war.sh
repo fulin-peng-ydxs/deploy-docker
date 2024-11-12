5103facca7b075ddc17a80a40770c1a87618ae02
pengfulin <1162855925@qq.com>
2024-11-12 15:29:40 +0800

    -x86 arm架构构建案例完善

--- src/main/resources/build_deploy/server/config/deploy_war.sh
+++ src/main/resources/build_deploy/server/config/deploy_war.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 docker build  --build-arg WAR_FILE=${1:-enforce-client.war} -t "reg.int.it2000.com.cn/com.aifa.mins/enforce/xhd:${2:-latest}" .
+
+#arm 架构构建
+#docker build  --build-arg WAR_FILE=${1:-enforce-client.war} -t  --platform linux/arm64  -f Dockerfile-arm "reg.int.it2000.com.cn/com.aifa.mins/enforce/xhd:${2:-latest}" .
